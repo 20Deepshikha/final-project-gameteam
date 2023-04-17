0b78e94947dee6e9a2c2f46cf8b13439d3c7508e
Reason Adhikari <72090971+adhikariReason@users.noreply.github.com>
2023-04-17 12:30:57 -0500

    added movements for car and added road

--- car.gd
+++ car.gd
@@ -1,12 +1,36 @@
 extends KinematicBody2D
 
 
+var accel = 5000
+var max_speed = 100000
+var turbo_speed = 1000000
+var turbo_accel = 10000
+var friction = 2500
+var deaccel = 10000
+const ROTATION_SPEED = 0.4
 
-# Called when the node enters the scene tree for the first time.
+var velocity = Vector2.ZERO
+onready var car = $AnimatedSprite
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	#speed and break
+	if(Input.is_action_pressed("ui_up")):
+		velocity.y = move_toward(velocity.y, -turbo_speed, turbo_accel * delta)
+	elif(Input.is_action_pressed("ui_up")):
+		velocity.y = move_toward(velocity.y, -max_speed, accel * delta)
+	elif(Input.is_action_pressed("ui_down")):
+		velocity.y = move_toward(velocity.y, 0, deaccel * delta)
+	else:
+		velocity.y = move_toward(velocity.y, 0, friction * delta)
+	
+	# Rotation of car
+	if Input.is_action_pressed("ui_left"):
+		rotation -= ROTATION_SPEED * delta
+	elif(Input.is_action_pressed("ui_right")):
+		rotation += ROTATION_SPEED * delta
+		
+	var motion = velocity * delta
+	move_and_slide(motion)
