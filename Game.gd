9b8d8e654eb6ec6231166778d97d4f130d071399
Deepshikha Bakshi <99354163+20Deepshikha@users.noreply.github.com>
2023-04-28 10:47:49 -0500

    updated script

--- Game.gd
+++ Game.gd
@@ -1,30 +1,45 @@
 extends Node2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var player : Node2D
+var score : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	# Find the player node in the scene tree
+	player = get_node("Player")
+	
+	# Set the starting score
+	score = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-func new_game():
-	$StaticBody2D.show()
-	$StaticBody2D/Heart1.show()
-	$StaticBody2D/Heart2.show()
-	$StaticBody2D/Heart3.show()
-	DeathCount = 0
-	get_tree().call_group("mobs", "queue_free")
-	$Music.play()
+# func _process(delta):
+#     pass
+
+func _on_HUD_start_game():
+	# Start the game by setting the player's position and resetting the score
+	player.position = Vector2(0, 0)
 	score = 0
-	$Player.start($StartPosition.position)
-	$StartTimer.start()
-	$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
-	$HUD/HighScore.hide()
+	
+	# Hide the HUD and show the game over screen
+	get_node("HUD").hide()
+	get_node("GameOverScreen").hide()
+
+func add_score(points : int):
+	# Add the specified number of points to the score
+	score += points
+	
+	# Update the score label in the HUD
+	get_node("HUD/ScoreLabel").text = "Score: " + str(score)
+
+func game_over():
+	# Show the game over screen and the final score
+	get_node("GameOverScreen/ScoreLabel").text = "Final Score: " + str(score)
+	get_node("GameOverScreen").show()
+
+	# Pause the game and disable the player's movement
+	get_tree().paused = true
+	player.get_node("Movement").set_process(false)
+	
