772955fd477e72570eab3dc0977496585a5716eb
Deepshikha Bakshi <99354163+20Deepshikha@users.noreply.github.com>
2023-04-28 23:32:44 -0500

    added coins

--- HUD.gd
+++ HUD.gd
@@ -1,7 +1,18 @@
 extends CanvasLayer
 signal start_game
 
+var coins = 0
 
+#func _ready():
+	#$coins.text = String(coins)
+	
+func _physics_process(delta):
+	if coins == 3:
+		get_tree().change_scene("res://Game.tscn")
+
+func _on_coin_collected():
+	coins = coins + 1
+	_ready()
 
 func show_message(text):
 	$Message.text = text
